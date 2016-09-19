6bcc385689ac8835fcba5358e4da4ca4b323ecc5
www-data <sunnysideupdevs@gmail.com>
2016-09-19 16:34:01 +1200

    fix ups

--- code/forms/CommentsFormAddRecaptcha.php
+++ code/forms/CommentsFormAddRecaptcha.php
@@ -2,10 +2,8 @@
 
 class CommentsFormAddRecaptcha extends Extension
 {
-
-    function alterCommentForm($form)
+    public function alterCommentForm($form)
     {
         $form->Fields()->push(RecaptchaField::create("Recaptcha"));
     }
-
 }
